3c4f9f9186ecf2b80ad4a327d2fa3225410d0adf
mirtabecker <mirta.atenea@gmail.com>
2018-12-05 16:18:08 -0300

    Modificacion metodos

--- src/institutoabierto/InstitutoAbierto.java
+++ src/institutoabierto/InstitutoAbierto.java
@@ -1,4 +1,3 @@
-
 package institutoabierto;
 
 import institutoAbiertoModelo.Conexion;
@@ -8,36 +7,35 @@ import institutoAbiertoModelo.Matricula;
 import institutoAbiertoModelo.MatriculaData;
 import institutoAbiertoModelo.Persona;
 import institutoAbiertoModelo.PersonaData;
-
+import java.sql.Date;
 
 public class InstitutoAbierto {
 
-    
     public static void main(String[] args) {
-    
-    Persona persona1 = new Persona ("Mirta Becker", 32693062, 4390907);   
-    Conexion con = new Conexion ();
-    PersonaData pd = new PersonaData (con);
-    
-    
-     pd.guardarPersona(persona1);
-     
-     System.out.println("Nueva Persona id: "+persona1.getId());
-     
-        
-     // TODO code application logic here
-    
-    
-     Curso curso1 = new Curso ("Matematicas", "Sistema de ecuaciones", 40, 200, persona1);
-     con = new Conexion();
-     CursoData cd = new CursoData (con);
-    
-    cd.guardarCurso(curso1);
-    
-    System.out.println("Nuevo Curso id: "+curso1.getId());
-    
-    
-    
-}
-}
 
+        Persona persona1 = new Persona("Mirta Becker", 32693062, 4390907);
+        Conexion con = new Conexion();
+        PersonaData pd = new PersonaData(con);
+
+        pd.guardarPersona(persona1);
+
+        System.out.println("Nueva Persona id: " + persona1.getId());
+
+        Persona persona2 = new Persona("Atenea Caselli", 33152586, 4365896);
+        con = new Conexion();
+        pd = new PersonaData(con);
+
+        pd.guardarPersona(persona2);
+
+        // TODO code application logic here
+        Curso curso1 = new Curso("Matematicas", "Sistema de ecuaciones", 40, 200, persona1);
+        con = new Conexion();
+        CursoData cd = new CursoData(con);
+
+        cd.guardarCurso(curso1);
+
+        System.out.println("Nuevo Curso id: " + curso1.getId());
+
+        
+    }
+}
